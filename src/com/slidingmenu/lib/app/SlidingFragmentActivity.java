27d996408119038df7aa8569fc9bcb9fb2d4a936
liushuaikobe <liushuaikobe@gmail.com>
2013-08-24 11:15:35 +0800

    fix contextmenu不触发滑屏 bug

--- src/com/slidingmenu/lib/app/SlidingFragmentActivity.java
+++ src/com/slidingmenu/lib/app/SlidingFragmentActivity.java
@@ -1,18 +1,21 @@
 package com.slidingmenu.lib.app;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.slidingmenu.lib.SlidingMenu;
 
-public class SlidingFragmentActivity extends FragmentActivity implements SlidingActivityBase {
+public class SlidingFragmentActivity extends SherlockFragmentActivity implements
+		SlidingActivityBase {
 
 	private SlidingActivityHelper mHelper;
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
 	 */
 	@Override
@@ -22,7 +25,9 @@ public class SlidingFragmentActivity extends FragmentActivity implements Sliding
 		mHelper.onCreate(savedInstanceState);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onPostCreate(android.os.Bundle)
 	 */
 	@Override
@@ -31,7 +36,9 @@ public class SlidingFragmentActivity extends FragmentActivity implements Sliding
 		mHelper.onPostCreate(savedInstanceState);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#findViewById(int)
 	 */
 	@Override
@@ -42,8 +49,12 @@ public class SlidingFragmentActivity extends FragmentActivity implements Sliding
 		return mHelper.findViewById(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see android.support.v4.app.FragmentActivity#onSaveInstanceState(android.os.Bundle)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.support.v4.app.FragmentActivity#onSaveInstanceState(android.os
+	 * .Bundle)
 	 */
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
@@ -51,7 +62,9 @@ public class SlidingFragmentActivity extends FragmentActivity implements Sliding
 		mHelper.onSaveInstanceState(outState);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#setContentView(int)
 	 */
 	@Override
@@ -59,16 +72,22 @@ public class SlidingFragmentActivity extends FragmentActivity implements Sliding
 		setContentView(getLayoutInflater().inflate(id, null));
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#setContentView(android.view.View)
 	 */
 	@Override
 	public void setContentView(View v) {
-		setContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		setContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.MATCH_PARENT));
 	}
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#setContentView(android.view.View, android.view.ViewGroup.LayoutParams)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#setContentView(android.view.View,
+	 * android.view.ViewGroup.LayoutParams)
 	 */
 	@Override
 	public void setContentView(View v, LayoutParams params) {
@@ -76,76 +95,105 @@ public class SlidingFragmentActivity extends FragmentActivity implements Sliding
 		mHelper.registerAboveContentView(v, params);
 	}
 
-	/* (non-Javadoc)
-	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(int)
 	 */
 	public void setBehindContentView(int id) {
 		setBehindContentView(getLayoutInflater().inflate(id, null));
 	}
 
-	/* (non-Javadoc)
-	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(android.view.View)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(android
+	 * .view.View)
 	 */
 	public void setBehindContentView(View v) {
-		setBehindContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		setBehindContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.MATCH_PARENT));
 	}
 
-	/* (non-Javadoc)
-	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(android.view.View, android.view.ViewGroup.LayoutParams)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(android
+	 * .view.View, android.view.ViewGroup.LayoutParams)
 	 */
 	public void setBehindContentView(View v, LayoutParams params) {
 		mHelper.setBehindContentView(v, params);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#getSlidingMenu()
 	 */
 	public SlidingMenu getSlidingMenu() {
 		return mHelper.getSlidingMenu();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#toggle()
 	 */
 	public void toggle() {
 		mHelper.toggle();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#showAbove()
 	 */
 	public void showContent() {
 		mHelper.showContent();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#showBehind()
 	 */
 	public void showMenu() {
 		mHelper.showMenu();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#showSecondaryMenu()
 	 */
 	public void showSecondaryMenu() {
 		mHelper.showSecondaryMenu();
 	}
 
-	/* (non-Javadoc)
-	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setSlidingActionBarEnabled(boolean)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.slidingmenu.lib.app.SlidingActivityBase#setSlidingActionBarEnabled
+	 * (boolean)
 	 */
 	public void setSlidingActionBarEnabled(boolean b) {
 		mHelper.setSlidingActionBarEnabled(b);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onKeyUp(int, android.view.KeyEvent)
 	 */
 	@Override
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
 		boolean b = mHelper.onKeyUp(keyCode, event);
-		if (b) return b;
+		if (b)
+			return b;
 		return super.onKeyUp(keyCode, event);
 	}
 
